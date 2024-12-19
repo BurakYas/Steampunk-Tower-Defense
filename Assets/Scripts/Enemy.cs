db184e6e7fda555f08dbaa1fc3071cfab24f729b
BurakYas <burakyasar749@gmail.com>
2024-12-19 10:54:34 +0300

    NavMeshAdded

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] private Transform[] waypoint;
+    private int waypointIndex;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    private void Update()
+    {
+        FaceTarget(agent.steeringTarget);
+
+        // Check if the agent has reached the destination
+        if (agent.remainingDistance < .5f)
+        {
+            // Check if the agent has reached the last waypoint
+            agent.SetDestination(GetNexWaypoint());
+        }
+    }
+
+    private void FaceTarget(Vector3 newTarget)
+    {
+        Vector3 directionTarget = newTarget - transform.position; // Get the direction to the target
+        directionTarget.y = 0; // Set the y axis to 0
+
+        Quaternion newRotation = Quaternion.LookRotation(directionTarget); // Get the rotation to look at the target
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime); // Rotate the enemy to look at the target smoothly
+    }
+
+    private Vector3 GetNexWaypoint()
+    {
+        if (waypointIndex >= waypoint.Length)
+        {
+            return transform.position;
+        }
+        
+        Vector3 targetPoint = waypoint[waypointIndex].position; // Get the next waypoint position
+        waypointIndex++; // Increase the waypoint index
+
+        return targetPoint;
+    }
+}
